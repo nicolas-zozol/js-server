d77bd6135e2246ce5b9f2d7239f5135af63364bc
Nicolas Zozol <nzozol@gmail.com>
2023-02-22 16:11:22 +0100

    Post and routes

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.get('/api/v1/service', (req, res) => {
     res.send('Running service is perfectly working!')
+})
+
+app.post('/api/v1/:protocol/:route', (req, res) => {
+    const protocol = req.params.protocol
+    const route = req.params.route
+    const content = req.body
+    content.protocol = protocol
+    content.route = route
+
+    console.log('sending back these data:', content)
+    res.send(content)
 })
 
 app.listen(port, () => {
