60103e9a7d1f8d31efd26836ca3878702ae30669
Nicolas Zozol <nzozol@gmail.com>
2023-02-23 17:13:30 +0100

    Quotes

--- server.js
+++ server.js
@@ -8,7 +8,24 @@ app.get('/api/v1/service', (req, res) => {
     res.send('Running service is perfectly working!')
 })
 
+app.post('/api/v1/quote', (req, res) => {
+    const url = req.url
+    console.log('/quote: getting url', url)
+    const protocol = req.query.protocol
+    const route = req.query.route
+    const content = req.body
+    content.protocol = protocol
+    content.route = route
+    content.url=url
+
+    console.log('sending back these data:', content)
+    res.send(content)
+})
+
 app.post('/api/v1/:protocol/:route', (req, res) => {
+    const url = req.url
+    console.log('/protocol/route: getting url', url)
+
     const protocol = req.params.protocol
     const route = req.params.route
     const content = req.body
@@ -18,6 +35,8 @@ app.post('/api/v1/:protocol/:route', (req, res) => {
     console.log('sending back these data:', content)
     res.send(content)
 })
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
